d97df240323f3c5f79ac916b880c00738a317fcd
Henrique Eduardo <103335396+NonDvcorDvco@users.noreply.github.com>
2022-07-25 16:48:58 -0300

    Add files via upload

---
+++ Beginner Template/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <string.h>
+
+using namespace std;
+
+#define max 10000
+
+void solve(){
+  /** write your solution here **/
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  solve();
+  return 0;
+}
